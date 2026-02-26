a051d13e59fe06aba6bb0cea810696dbfe7b53fc
Shivanimyadam <shivanimyadam@gmail.com>
2026-02-26 21:01:44 +0530

    ENH: Added register and login apis.
    - Implemented register and login api route.
    - Using bcrypt encrypted password.

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,23 @@ const PORT = 4000;
 
 const db = require('./db');
 
+const bcrypt = require('bcryptjs');
+
 //Middleware
 
 app.use(cors());
 app.use(express.json());
 
 //Test Route
-app.get('/api/health', (req,res)=>{
+app.get('/api/health', (req, res) => {
     res.send("API Running...");
 });
 
 // Get all products
-app.get('/api/products', (req,res) => {
-    db.query('SELECT * FROM products',(err,results) => {
-        if(err){
-            res.status(500).json({error: err.message});
+app.get('/api/products', (req, res) => {
+    db.query('SELECT * FROM products', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
             return;
         }
         res.json(results);
@@ -28,33 +30,33 @@ app.get('/api/products', (req,res) => {
 });
 
 // Post products
-app.post('/api/products',(req,res)=>{
+app.post('/api/products', (req, res) => {
     const { name, category, price, stock, status } = req.body;
 
     const sql = 'INSERT INTO products (name, category, price, stock, status) VALUES (?, ?, ?, ?, ?)';
 
     db.query(sql, [name, category, price, stock, status], (err, result) => {
-        if(err){
-            res.status(500).json({error: err.message});
+        if (err) {
+            res.status(500).json({ error: err.message });
             return;
         }
-        res.json({message: "Product added successfully!", id: result.insertId});
+        res.json({ message: "Product added successfully!", id: result.insertId });
     });
 });
 
 // Update product
-app.put('/api/products/:id', (req,res)=>{
+app.put('/api/products/:id', (req, res) => {
     const { id } = req.params;
     const { name, category, price, stock, status } = req.body;
 
     const sql = 'UPDATE products SET name=?, category=?, price=?, stock=?, status=? WHERE id=?';
 
-    db.query(sql, [name, category, price, stock, status, id], (err, result) =>{
-        if(err){
-            res.status(500).json({error: err.message});
+    db.query(sql, [name, category, price, stock, status, id], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
             return;
         }
-        res.json({message: "Product updated successfully!"});
+        res.json({ message: "Product updated successfully!" });
     });
 });
 
@@ -63,7 +65,7 @@ app.delete('/api/products/:id', (req, res) => {
     const { id } = req.params;
 
     db.query('DELETE FROM products WHERE id=?', [id], (err, result) => {
-        if(err){
+        if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
@@ -71,7 +73,46 @@ app.delete('/api/products/:id', (req, res) => {
     });
 });
 
+// Register User
+app.post('/api/register', (req, res) => {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const sql = 'INSERT INTO users(username, email, password) VALUES ( ?, ?, ?)';
+
+    db.query(sql, [username, email, hashedPassword], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({ message: 'User registered successfully!' });
+    });
+});
+
+//Login user
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+    db.query('SELECT * FROM users WHERE email=?', [email], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        if (result.length === 0) {
+            res.status(401).json({ message: 'Invalid email or password' });
+            return;
+        }
+        const user = result[0];
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            res.status(401).json({ message: 'Invalid email or password' });
+            return;
+        }
+        res.json({ message: "Login successfull!", user: { id: user.id, username: user.username, email: user.email } });
+    });
+});
+
 //Start server
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
